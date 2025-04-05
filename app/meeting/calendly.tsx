6297428aa635bff0c4701965f1d6b66aaf23e17d
asr <ankitasinharay3@gmail.com>
2025-04-05 21:27:01 +0530

    calendly

--- app/meeting/calendly.tsx
+++ app/meeting/calendly.tsx
@@ -5,12 +5,19 @@ import { InlineWidget } from "react-calendly";
 
 const Calendly =  () => {
 
+    // return ( 
+    // <div className="w-full ">
+    //   <InlineWidget
+    //   styles={{ height: "1000px" }}
+    //   url="https://calendly.com/birdsoftware/meeting" />
+    // </div> );
     return ( 
-    <div className="w-full ">
-      <InlineWidget
-      styles={{ height: "1000px" }}
-      url="https://calendly.com/birdsoftware/meeting" />
-    </div> );
+      <div className="w-full ">
+        <InlineWidget
+        styles={{ height: "1000px" }}
+        url="https://calendly.com/softoratech/30min" />
+        
+      </div> );
 }
  
 export default Calendly;
