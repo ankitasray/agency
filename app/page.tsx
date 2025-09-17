8b7ac21faeb7c89de7278d822dc544bc20765d29
asr <ankitasinharay3@gmail.com>
2025-09-17 21:12:01 +0530

    Revert "update home"
    This reverts commit 8dd6baeca0f9bffe651879b4072a3083c13140de.

--- app/page.tsx
+++ app/page.tsx
@@ -172,7 +172,7 @@ export default function Home() {
             >
               Services
             </ScrollLink>
-            
+
             <ScrollLink
               to="process"
               smooth={true}
